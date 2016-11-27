66a5f534bf6a28bf306be76e7df14d49445e41de
Alex Efremov <alex.efremov.dev@gmail.com>
2016-11-27 23:24:59 +0200

    Add manager project

---
+++ manager/src/app.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { View, Text } from 'react-native';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+import { firebaseConfig } from './config';
+
+class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>
+            Hello!
+          </Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+export default App;

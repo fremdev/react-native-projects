41e7ee08df794d798bc684e895c29ffd4732de11
Alex Efremov <alex.efremov.dev@gmail.com>
2016-11-28 11:10:18 +0200

    Add LoginForm component

---
+++ manager/src/components/common/Spinner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, ActivityIndicator, StyleSheet } from 'react-native';
+
+export const Spinner = (props) => {
+  return (
+    <View style={styles.spinnerStyle}>
+      <ActivityIndicator size={props.size || 'large'} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  spinnerStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});

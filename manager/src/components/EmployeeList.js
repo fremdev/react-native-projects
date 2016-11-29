ff2e33dc15f1c778f81580baec24fa0c86636b90
Alex Efremov <alex.efremov.dev@gmail.com>
2016-11-29 18:22:45 +0200

    Add EmployeeListItem and render list with data from firebase

--- manager/src/components/EmployeeList.js
+++ manager/src/components/EmployeeList.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { ListView, View, Text } from 'react-native';
 import { employeesFetch } from '../actions';
+import EmployeeListItem from './EmployeeListItem';
 
 class EmployeeList extends Component {
   componentWillMount() {
     this.props.employeesFetch();
+
+    this.createDataSource(this.props);
   }
-  
+
+  componentWillReceiveProps(nextProps) {
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource({ employees }) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(employees);
+  }
+
+  renderRow(employee) {
+    return <EmployeeListItem employee={employee} />;
+  }
+
   render() {
     return (
-      <View>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-      </View>
+      <ListView
+        enableEmptySections
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
     );
   }
 }
 
-export default connect(null, { employeesFetch })(EmployeeList);
+const mapStateToProps = state => {
+  return { employees: state.employees };
+};
+
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
---
+++ manager/src/components/EmployeeListItem.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Text, StyleSheet } from 'react-native';
+import { CardSection } from './common';
+
+class EmployeeListItem extends Component {
+  render() {
+    const { name } = this.props.employee;
+
+    return (
+      <CardSection>
+        <Text style={styles.titleStyle}>
+          {name}
+        </Text>
+      </CardSection>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15
+  }
+});
+
+export default EmployeeListItem;

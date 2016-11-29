b60fdc493558692c434f1ba271d1580d0a0cbc3e
Alex Efremov <alex.efremov.dev@gmail.com>
2016-11-30 00:05:20 +0200

    Add reusable Confirm component

--- manager/src/components/EmployeeEdit.js
+++ manager/src/components/EmployeeEdit.js
@@ -3,10 +3,12 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { text } from 'react-native-communications';
 import { employeeUpdate, employeeSave } from '../actions';
-import { Card, CardSection, Button } from './common';
+import { Card, CardSection, Button, Confirm } from './common';
 import EmployeeForm from './EmployeeForm';
 
 class EmployeeEdit extends Component {
+  state = { showModal: false };
+
   componentWillMount() {
     _.each(this.props.employee, (value, prop) => {
       this.props.employeeUpdate({ prop, value });
@@ -37,6 +39,16 @@ class EmployeeEdit extends Component {
         <CardSection>
           <Button text="Text Schedule" onPress={this.onTextPress.bind(this)} />
         </CardSection>
+
+        <CardSection>
+          <Button text="Fire Employee" onPress={() => this.setState({ showModal: !this.state.showModal })} />
+        </CardSection>
+
+        <Confirm
+          visible={this.state.showModal}
+        >
+          Are you sure you want to delete this?
+        </Confirm>
       </Card>
     );
   }
